a63d0c09949addafb486853bfb3cc8dc8c41dce2
devdan <oluochdancun19@gmail.com>
2022-06-17 10:35:16 +0300

    commit tree ced12f911bdd6df43d0dcc9aac5de5d7b99787d5

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+// const newarr = drivers.slice(0, 2)
+// console.log(newarr)
+
+const returnFirstTwoDrivers =function(drivers){
+    // const newdrivers =drivers.slice(0, 2)
+    
+    // console.log(newdrivers)
+        return drivers.slice(0, 2);
+   
+}
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+  };
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  const createFareMultiplier = function (multiplyValue) {
+    return function (value) {
+      return multiplyValue * value;
+    };
+  };
